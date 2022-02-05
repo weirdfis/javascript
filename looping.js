9b14a1961b684723f5b44702820c9c85b2a2a398
aqil <ghilman.m.aqil@gmail.com>
2022-02-05 20:07:44 +0700

    membuat file looping

---
+++ looping.js
@@ -0,0 +1,15 @@
+function FOR() {
+  for (var i = 0; i < 10; i++) {
+    console.log("urutan ke ", i);
+  }
+}
+function WHILE() {
+  let i = 1;
+  while (i<10) {
+    console.log("ke-", i);
+    i++;
+  }
+}
+
+FOR()
+WHILE()
