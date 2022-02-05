84b6a37dd881a0cacbcd48f42168a1c7da27e005
aqil <ghilman.m.aqil@gmail.com>
2022-02-05 16:43:36 +0700

    membuat file if-else

---
+++ if-else.js
@@ -0,0 +1,16 @@
+function jikaa() {
+  var nilai = 8;
+  if(nilai===8){
+    console.log("delapan");
+  }
+  else {
+    console.log("gk tau")
+  }
+}
+function makaa() {
+  var angka = 9;
+  if (var=9)console.log("sembilan")
+  else console.log("gatau");
+}
+jikaa()
+makaa()
