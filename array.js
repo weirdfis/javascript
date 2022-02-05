cfcb24002f5d1ae90ca175164b19daee61694e70
aqil <ghilman.m.aqil@gmail.com>
2022-02-06 06:03:51 +0700

    menambhakan file array

---
+++ array.js
@@ -0,0 +1,14 @@
+function memanggilarray() {
+  var dataNama = [["satu","dua","tiga"],
+                  ["pertama","kedua","ketiga"],
+                  ["one","two","three"]]
+  console.log(dataNama.length);
+  console.log(dataNama[0][2]);
+}
+
+memanggilarray()
+
+var data = [8,9,0];
+for (var i = 0; i < data.length; i++) {
+  console.log(data[i]);
+}
