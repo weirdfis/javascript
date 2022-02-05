51088f668b21c4b5517a3c58402c7b0bea59e380
aqil <ghilman.m.aqil@gmail.com>
2022-02-05 14:14:13 +0700

    membuat function

---
+++ function.js
@@ -0,0 +1,13 @@
+function fungsi1() {
+  console.log("pertama");
+}
+const fungsi2 = function() {
+  console.log("kedua");
+}
+const fungsi3 = ()=>{
+  console.log("ketiga");
+}
+
+fungsi1()
+fungsi2()
+fungsi3()
