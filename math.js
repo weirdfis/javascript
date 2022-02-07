25f6cb4400e4ec512488c454256511ab6c3154dd
aqil <ghilman.m.aqil@gmail.com>
2022-02-07 22:04:15 +0700

    menambahkan method math

--- math.js
+++ math.js
@@ -6,3 +6,13 @@ console.log(Math.LOG2E); //angka logaritma natural e basis 2
 console.log(Math.PI); //berisi angka pi 3.14...
 console.log(Math.SQRT1_2); //angka 1 dibagi dengan akar kuadrat 2
 console.log(Math.SQRT2); //angka akar kuadrat 2
+
+Math.ceil() //pembulatan ke atas
+Math.floor() //pembulatan ke bawah
+Math.round() //pembulatan biasa
+Math.abs() //absolut, mutlak
+Math.pow(x,y) // x pangkat y
+Math.sqrt() //akar
+Math.logx() //logaritma x
+Math.min(x,y,z) //nilai terendah
+Math.max(x,y,z) //nilai tertinggi
