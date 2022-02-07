d9fda096e3c5a65a912bdb31654e76a89060065c
aqil <ghilman.m.aqil@gmail.com>
2022-02-07 18:59:04 +0700

    menambahkan fungsi array shift

--- array.js
+++ array.js
@@ -12,3 +12,19 @@ var data = [8,9,0];
 for (var i = 0; i < data.length; i++) {
   console.log(data[i]);
 }
+
+function arrayshift() {
+  var kota = ['jakarta', 'bandung','bekasi'];
+  console.log(kota2);
+  console.log("=============");
+  var kota2 = kota.shift();
+  /*
+  kota.pop()
+  kota.push('depok')
+  kota.splice(2,1,'palembang')
+  kota.unshift('joga','malang')
+  */
+  console.log(kota2)
+
+  return kota
+}
